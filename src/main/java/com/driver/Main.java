a30bf040840bac0be88f16db105b56eb97aa378b
Sowndharya <sowndharyaktr@gmail.com>
2023-01-13 08:18:26 +0530

    changed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,22 +1,24 @@
 package com.driver;
 
 public class Main {
-     Main() {
+
+     public Main() {
         Product p=new Product();
         p.product(2,3);
         p.product(2,3,4);
         p.product(0.2,0.3);
     }
-}
+    class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
 
-class Product{
-    public int product(int x, int y) {
-        return x*y;
     }
-    public int product(int x, int y, int z) {
-        return x*y*z;
-    }
-    public double product(double x, double y) {
-        return x*y;
-    }
+
 }
