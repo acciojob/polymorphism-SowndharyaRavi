96bae8735564ab0c03626aaac944511df8b9286c
Sowndharya <sowndharyaktr@gmail.com>
2023-01-13 07:56:18 +0530

    made changes in polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,14 +2,14 @@ package com.driver;
 
 public class Main {
     public static void main(String [] args) {
-        product p=new product();
+        Product p=new Product();
         p.product(2,3);
         p.product(2,3,4);
         p.product(0.2,0.3);
     }
 }
 
-class product{
+class Product{
     public int product(int x, int y) {
         return x*y;
     }
