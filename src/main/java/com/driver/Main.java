5a7831282bcad8bae28517809b3351dab21ee837
Sowndharya <sowndharyaktr@gmail.com>
2023-01-13 07:47:45 +0530

    first polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
+    public static void main(String [] args) {
+        product p=new product();
+        p.product(2,3);
+        p.product(2,3,4);
+        p.product(0.2,0.3);
+    }
+}
 
+class product{
+    public int product(int x, int y) {
+        return x*y;
+    }
+    public int product(int x, int y, int z) {
+        return x*y*z;
+    }
+    public double product(double x, double y) {
+        return x*y;
+    }
 }
